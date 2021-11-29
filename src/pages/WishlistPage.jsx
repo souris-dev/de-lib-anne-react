71ba6512d61c6d033f9dd784a7d0eb3efc9cf7b4
souris-dev <suorees113@gmail.com>
2021-11-29 09:01:37 +0530

    Wishlist page connected to backend

--- src/pages/WishlistPage.jsx
+++ src/pages/WishlistPage.jsx
@@ -2,56 +2,71 @@ import { useEffect, useState } from "react";
 import { Tags, Stars } from "../components/BookDesc/Components";
 import Fade from "react-reveal/Fade.js";
 import { Link } from "react-router-dom";
+import DefaultLoadingScreen from "../components/Loaders/DefaultLoadingScreen";
 
 import "./WishlistPage.css";
 
 import { ThemeContext } from "../contexts/ThemeProvider";
 import { useContext } from "react";
+import {
+  atServiceEndpoint,
+  getData,
+  postData,
+} from "../utils/serverFetchUtils";
 
 export function WishlistPage() {
   const [bookDescs, setBookDescs] = useState([]);
   const { themeData: theme } = useContext(ThemeContext);
 
+  const [dataLoaded, setDataLoaded] = useState(false);
+
   useEffect(() => {
-    setBookDescs([
-      {
-        bookTitle: "Anxious People",
-        author: "Frederik Backman",
-        nstars: 3,
-        isbn13: "9781797105826",
-        olid: "OL28571780M",
-        tags: ["Fiction", "Drama", "Feel good"],
-      },
-      {
-        bookTitle: "The Guest List",
-        author: "Lucy Foley",
-        nstars: 4,
-        isbn13: "9780008297183",
-        olid: "OL29926388M",
-        tags: ["Mystery", "Suspense", "Thriller"],
-      },
-      {
-        bookTitle: "The Family Upstairs",
-        author: "Lisa Jewell",
-        nstars: 3,
-        isbn13: "9781797113821",
-        olid: "OL32009030M",
-        tags: ["Thriller", "Mystery", "Suspense"],
-      },
-    ]);
+    getData(atServiceEndpoint("wishlist", "/getwishlist"))
+      .then((response) => {
+        if (response.userWishlist) {
+          setBookDescs(response.userWishlist);
+          setDataLoaded(true);
+        }
+      })
+      .catch((error) => console.log(error));
   }, []);
 
+  const removeWishlistItem = (isbn13) => {
+    postData(
+      atServiceEndpoint("wishlist", "/deletewishitem"),
+      {
+        isbn13: isbn13,
+      },
+      "DELETE"
+    ).then((response) => {
+      if (response.status == 200) {
+        setBookDescs(bookDescs.filter((book) => book.isbn13 != isbn13));
+      }
+    });
+  };
+
+  if (!dataLoaded) {
+    return <DefaultLoadingScreen />;
+  }
+
   return (
-    <section className="text-gray-400 body-font">
-      <h1 className={`text-4xl font-medium title-font ${theme.dark ? "text-white" : "text-gray-800"} mt-10 text-center`}>
-        Souris' <span className={theme.dark ? `font-thin` : `font-bold`}>Wishlist</span>
+    <section className="text-gray-400 body-font min-h-screen">
+      <h1
+        className={`text-4xl font-medium title-font ${
+          theme.dark ? "text-white" : "text-gray-800"
+        } mt-10 text-center`}
+      >
+        Souris'{" "}
+        <span className={theme.dark ? `font-thin` : `font-bold`}>Wishlist</span>
       </h1>
       <Fade bottom cascade>
         <div className="py-24 mx-auto">
           {bookDescs.map((bookDesc) => (
             <div
               key={bookDesc.isbn13}
-              className={`flex items-center lg:w-4/5 border-b pb-10 mb-10 ${theme.dark ? "border-gray-800" : "border-wishlist-book"} mx-5 sm:mx-10 md:mr-16 lg:mx-auto md:flex-row flex-col`}
+              className={`flex items-center lg:w-4/5 border-b pb-10 mb-10 ${
+                theme.dark ? "border-gray-800" : "border-wishlist-book"
+              } mx-5 sm:mx-10 md:mr-16 lg:mx-auto md:flex-row flex-col`}
             >
               <div className="sm:mr-10 md:mr-3 inline-flex items-center w-full sm:w-1/2 md:w-1/3 justify-center text-yellow-400 flex-shrink-0">
                 <img
@@ -66,15 +81,25 @@ export function WishlistPage() {
                 />
               </div>
               <div className="lg:w-1/2 w-full lg:pl-4 lg:py-6 mt-10 sm:mt-6 lg:mt-0">
-                <h1 className={`${theme.dark ? "text-white" : "text-gray-800"} text-3xl title-font font-medium mb-1`}>
-                  <Link to={`/books/${bookDesc.bookTitle}`}>
-                    {bookDesc.bookTitle}
-                  </Link>
+                <h1
+                  className={`${
+                    theme.dark ? "text-white" : "text-gray-800"
+                  } text-3xl title-font font-medium mb-1`}
+                >
+                  <Link to={`/books/${bookDesc.isbn13}`}>{bookDesc.title}</Link>
                 </h1>
-                <h2 className={`text-base title-font mt-2 ${theme.dark ? "text-gray-500" : "text-gray-800"} tracking-widest`}>
+                <h2
+                  className={`text-base title-font mt-2 ${
+                    theme.dark ? "text-gray-500" : "text-gray-800"
+                  } tracking-widest`}
+                >
                   {bookDesc.author}
                 </h2>
-                <h2 className={`text-sm title-font mt-2 ${theme.dark ? "text-gray-500" : "text-gray-600"} tracking-widest inline-block`}>
+                <h2
+                  className={`text-sm title-font mt-2 ${
+                    theme.dark ? "text-gray-500" : "text-gray-600"
+                  } tracking-widest inline-block`}
+                >
                   ISBN13: {bookDesc.isbn13}
                 </h2>
                 <div className="flex mb-4 mt-2">
@@ -86,7 +111,12 @@ export function WishlistPage() {
                   <Tags tags={bookDesc.tags} />
                 </div>
                 <div className="flex mt-4">
-                  <button className={`flex ml-auto ${theme.dark ? "text-white" : "text-white bg-red-500"} transition-all duration-500 border border-red-600 py-2 px-6 focus:outline-none hover:bg-red-700 hover:text-white rounded text-base`}>
+                  <button
+                    onClick={() => removeWishlistItem(bookDesc.isbn13)}
+                    className={`flex ml-auto ${
+                      theme.dark ? "text-white" : "text-white bg-red-500"
+                    } transition-all duration-500 border border-red-600 py-2 px-6 focus:outline-none hover:bg-red-700 hover:text-white rounded text-base`}
+                  >
                     Remove
                   </button>
                 </div>
